7d4ad86e482092615487b93a3e4697704cae6552
awa2 <tawatsu@c-fo.com>
2019-06-23 09:50:54 +0900

    Add sum functions

--- src/index.ts
+++ src/index.ts
@@ -3,10 +3,12 @@ import { process, EventStore } from './Process';
 import { SubmitEvent } from './SubmitEvent';
 import EventObject from './types/EventObject';
 import ConfigTemplate from './Config';
+import { md5sum, sha1sum, sha256sum, sha384sum, sha512sum } from './Hash';
 
 export { AutoFoldering }
 export { EventStore }
 export { SubmitEvent }
-export { EventObject}
+export { EventObject }
 export { process }
 export { ConfigTemplate }
+export { md5sum, sha1sum, sha256sum, sha384sum, sha512sum }
